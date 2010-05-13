c33a2b20de81c0d4eb65476c64ab1f7a53a90d1b
André Fiedler <fiedler.andre@gmail.com>
2010-05-13 23:36:03 +0200

    ups, forgot somethin...

--- Source/Fx.Tween.CSS3.Replacement.js
+++ Source/Fx.Tween.CSS3.Replacement.js
@@ -53,18 +53,6 @@ Element.implement({
 
 });
 
-Object.prototype.clone = function() {
-	
-	var newObj = (this instanceof Array) ? [] : {};
-	for (i in this) {
-		if (i == 'clone') continue;
-		if (this[i] && typeof this[i] == "object") {
-			newObj[i] = this[i].clone();
-		} else newObj[i] = this[i]
-	} return newObj;
-		
-});
-
 Fx.TweenCSS2 = Fx.Tween;
 
 Fx.Tween.CSS3 = new Class({
